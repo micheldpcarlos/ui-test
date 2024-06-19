cd4997fb5b3281b2eda01a681211ee38a0b6b848
Michel Carlos <mcarlos@littleotterhealth.com>
2024-06-19 18:35:35 -0300

    initial voteItem implementation

---
+++ src/components/VoteItem.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps(['voteItem'])
+</script>
+
+<template>
+  <div class="vote-item">
+    <img class="vote-item__picture" :src="`/pictures/${voteItem.picture}`" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.vote-item {
+  width: 300px;
+  height: 300px;
+  flex: 0 0 auto;
+  position: relative;
+
+  &__picture {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+}
+</style>
--- src/views/PreviousRulingsView.vue
+++ src/views/PreviousRulingsView.vue
@@ -1,11 +1,61 @@
 <script setup>
-import data from '../data/data.json'
+import { onMounted, ref, watch } from 'vue'
+import ModeSelector from '@/components/ModeSelector.vue'
+import { useWindowSize } from '@vueuse/core'
 
-console.log(data)
+import data from '../data/data.json'
+import VoteItem from '@/components/VoteItem.vue'
+const { width } = useWindowSize()
+
+const selectedViewMode = ref('list')
+
+watch(
+  width,
+  (newValue) => {
+    if (newValue < 768) selectedViewMode.value = 'grid'
+  },
+  { immediate: true }
+)
 </script>
 
 <template>
-  <div>
-    <h2>Previous Rulings</h2>
+  <div class="previous-rulings">
+    <div class="previous-rulings__header">
+      <h2>Previous Rulings - {{ selectedViewMode }}</h2>
+      <ModeSelector v-model="selectedViewMode" class="previous-rulings__selector" />
+    </div>
+    <div class="previous-rulings__items">
+      <VoteItem v-for="item in data" :vote-item="item" :key="item.name" />
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.previous-rulings {
+  &__header {
+    position: relative;
+  }
+
+  .previous-rulings__selector {
+    display: none;
+  }
+
+  &__items {
+    display: flex;
+    overflow: auto;
+    gap: 1rem;
+    margin-right: -1rem;
+  }
+}
+
+@media all and (min-width: 768px) {
+  .previous-rulings {
+    .previous-rulings__selector {
+      display: block;
+      position: absolute;
+      bottom: 0;
+      right: 0;
+    }
+  }
+}
+</style>
